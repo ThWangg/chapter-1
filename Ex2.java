b9c387ff5161cb76c94aa7ba50a53671f377aa35
Admin <quangphanthanhnguyen@gmail.com>
2024-09-19 00:01:13 +0700

    Exercise 2

---
+++ Ex2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ex2 {
+    public static void main(String[] args){
+        Scanner k = new Scanner(System.in);
+        System.out.println("input a line:");
+        int n=k.next().length();
+        System.out.println("number of characters:"+n);
+    }
+}
